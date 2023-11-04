bd6810fd92048d378a7d261bc9cfdd04035903ef
Bao Huynh <55563202+bhuynhdev@users.noreply.github.com>
2023-11-03 22:23:52 -0400

    HomePage loader for modules and assignments

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,7 +4,7 @@ export const HomagePage = () => {
   const { modules, assignments } = useLoaderData();
 
   return (
-    <main className="grid grid-cols-[7%_1fr_20%] px-8">
+    <main className="grid grid-cols-[7%_1fr_20%] px-8 gap-12">
       <nav>
         <ul className="flex flex-col gap-3 py-6">
           {["Home", "Groups", "Inbox", "Assignments", "Grades", "Syllabus"].map((tabName) => (
@@ -14,18 +14,27 @@ export const HomagePage = () => {
           ))}
         </ul>
       </nav>
-      <div></div>
-      <aside className="border-2 border-orange-700">
+      <div className="px-24 py-6 flex gap-6 flex-col">
+        {modules.map((module) => (
+          <details open>
+            <summary>{module.title}</summary>
+            <ul>
+              {module.items.map((moduleItem) => (
+                <li>
+                  <Link to={`${moduleItem.slug}`}>{moduleItem.title}</Link>
+                  </li>
+              ))}
+            </ul>
+          </details>
+        ))}
+      </div>
+      <aside className="py-6 border-2 border-orange-700">
         <ul>
-          <li className="h-16">
-            <Link to="">Homework Item</Link>
-          </li>
-          <li className="h-16">
-            <Link to="">Homework Item</Link>
-          </li>
-          <li className="h-16">
-            <Link to="">Homework Item</Link>
-          </li>
+          {assignments.map((assignment) => (
+            <li className="h-16">
+              <Link to="">{assignment.title}</Link>
+            </li>
+          ))}
         </ul>
       </aside>
     </main>
