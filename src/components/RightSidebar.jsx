3439d89e10f79fe0e6eae942c7fb94905c3ea916
Josh McKettrick <mckettja@mail.uc.edu>
2023-11-10 11:31:39 -0500

    horizontal sliding home page

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 export const RightSidebar = ({ assignmentList }) => {
   return (
     <aside className="py-6 border-2 border-orange-700 w-72">
-      <button>OPEN TRACKER</button>
       <ul>
         {assignmentList.map((assignment) => (
           <li className="h-16">
