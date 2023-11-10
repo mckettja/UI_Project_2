204d625b90a156113a0ac39ab53560ac948cefcd
Josh McKettrick <mckettja@mail.uc.edu>
2023-11-10 10:39:48 -0500

    create object

---
+++ src/components/AssignmentTracker.jsx
@@ -0,0 +1,11 @@
+
+
+export const AssignmentTracker () {
+    
+
+    return (
+
+    )
+}
+
+export default AssignmentTracker;
--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 export const RightSidebar = ({ assignmentList }) => {
   return (
     <aside className="py-6 border-2 border-orange-700 w-72">
+      <button>OPEN TRACKER</button>
       <ul>
         {assignmentList.map((assignment) => (
           <li className="h-16">
