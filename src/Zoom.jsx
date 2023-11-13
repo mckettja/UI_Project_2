59e9cf05c90adf2c54ae30ac731bbdd31dd58700
Josh McKettrick <mckettja@mail.uc.edu>
2023-11-13 13:27:41 -0500

    final changes

--- src/Zoom.jsx
+++ src/Zoom.jsx
@@ -28,18 +28,46 @@ export const Zoom = () => {
       style={{
         display: "flex",
         flexDirection: "column", 
-        overflow: "auto",
-        maxWidth: "1000px",
       }}
       onWheel={handleWheel}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <h1 style={{ fontSize: '1.5em', textDecoration: 'underline' }}><strong>Zoom Page</strong></h1>
-      <br></br>
-      <p>Here is where the link to our zoom meeting will be, click the link below and your page will be rerouted to a new page.</p>
-      <br></br>
-      <a style={{ fontSize: '1.2em', color: 'blue', fontWeight: 'bold', textDecoration: 'underline' }} href="https://ucincinnati.zoom.us/j/92459304063?pwd=RjRXcGI3RE9oS1VvbStkQzlVTWg1dz09&23=#success" target="_blank"><p>Click Here</p></a>
+      <h1
+        style={{
+          fontSize: '1.5em',
+          textDecoration: 'underline',
+          marginBottom: '10px', // Add some space below the title
+        }}
+      >
+        <strong>Zoom Page</strong>
+      </h1>
+      <br />
+      <p>
+        Here is where the link to our Zoom meeting will be. Click the link below, and your page will be rerouted to a new page.
+      </p>
+      <br />
+      <a
+        href="https://ucincinnati.zoom.us/j/92459304063?pwd=RjRXcGI3RE9oS1VvbStkQzlVTWg1dz09&23=#success"
+        target="_blank"
+        style={{
+          fontSize: '1.2em',
+          display: 'inline-block', // Make the link text inline
+        }}
+      >
+        <span
+          style={{
+            color: 'white', // Text color
+            fontWeight: 'bold',
+            textDecoration: 'none', // Remove underline for a button-like appearance
+            backgroundColor: 'rgb(0, 123, 255)', // Background color for the button
+            padding: '10px 20px', // Padding inside the button
+            borderRadius: '5px', // Rounded corners for the button
+          }}
+        >
+          Click Here
+        </span>
+      </a>
     </div>
   );
 };
