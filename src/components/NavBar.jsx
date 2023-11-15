3860627d38198206f5fff6b7900d8d26e5c1bd33
Bao Huynh <55563202+bhuynhdev@users.noreply.github.com>
2023-11-14 20:27:49 -0500

    Fix week number display

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,18 @@
+import dayjs from "dayjs"
 import { useMyStoreState } from "../store"
 import AnimalIcon from "./AnimalIcon"
 
+const SEMESTER_START_DATE = "2022-08-21"
+
 export const NavBar = ({ courseList }) => {
 	const user = useMyStoreState((state) => state.user)
-	const currentDate = useMyStoreState(state => state.currentDate)
+	const currentDate = useMyStoreState((state) => state.currentDate)
 
 	return (
 		<nav className="relative flex h-full flex-1 justify-center px-12">
-			<div className="absolute left-0 top-0 p-[inherit] flex flex-col gap-2 justify-center h-full">
+			<div className="absolute left-0 top-0 flex h-full flex-col justify-center gap-2 p-[inherit]">
 				<p>{user.name}</p>
-				<p>Week: 8</p>
+				<p>Week: {dayjs(currentDate).diff(dayjs(SEMESTER_START_DATE), "week")}</p>
 				<p>Date: {currentDate.format("MM/DD/YYYY")}</p>
 				<p></p>
 			</div>
